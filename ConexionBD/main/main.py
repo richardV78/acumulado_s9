59af76c6a24c0319487e7b14db2f372d974ced22
richard <richardvalles94@gmail.com>
2024-03-22 21:04:16 -0600

    si

---
+++ ConexionBD/main/main.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+class Connection:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.cnx = None
+        self.connect()
+        
+    def connect(self):
+        self.cnx = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
+    
